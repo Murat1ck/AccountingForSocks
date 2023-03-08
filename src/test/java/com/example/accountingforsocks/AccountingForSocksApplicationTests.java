4832d966578824836d146f51cd47410482a73d5b
Muratov <muratoff101088@gmail.com>
2023-03-08 12:45:02 +0300

    Первый коммит

---
+++ src/test/java/com/example/accountingforsocks/AccountingForSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountingforsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingForSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
